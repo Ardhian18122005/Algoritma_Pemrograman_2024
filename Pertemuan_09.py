700f1ba444054d94c9a4328fbb8af7920a30b572
Ardhian18122005 <anandaardhian18@gmail.com>
2024-10-21 05:07:47 -0700

    Add files via upload

---
+++ Pertemuan_09.py
@@ -0,0 +1,20 @@
+a = 10
+b = 5
+hasil_int = a + b
+
+x = 3.14
+y = 2.5
+hasil_float = x * y
+
+n = "ardhian"
+u = "18"
+hasil_str = "Nama: " + n + " , Umur: " + u
+
+b = True
+s = False
+hasil_bool = b or s
+
+print("Hasil Integer: " , hasil_int)
+print("Hasil Float: " , hasil_float)
+print("Hasil String: " , hasil_str)
+print("Hasil Boolean: " , hasil_bool)
